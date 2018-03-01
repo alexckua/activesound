ce11f703bc89a625405a24d565643bc9390a170b
alexckua <alex@net.ck.ua>
2018-03-02 00:25:28 +0200

    Add file uploader

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,3 @@
 class Track < ApplicationRecord
+  mount_base64_uploader :file, AudioUploader
 end
---
+++ app/uploaders/audio_uploader.rb
@@ -0,0 +1,21 @@
+require 'carrierwave/audio'
+
+class AudioUploader < CarrierWave::Uploader::Base
+  include CarrierWave::Audio
+
+  MIME::Types.add(
+    MIME::Type.new('audio/mp3').tap do |type|
+      type.add_extensions 'mp3'
+    end
+  )
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w( mp3 )
+  end
+end
