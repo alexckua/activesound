b50e24d1c79593131f37938344da79666e362062
alexckua <alex@net.ck.ua>
2018-03-02 23:40:04 +0200

    Add SignIn button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require rails-ujs
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require popper
+//= require bootstrap
 //= require_tree .
