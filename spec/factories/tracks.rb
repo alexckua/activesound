abc8abe4809bfb20d7f1f709a33fb5aabcd28def
Alex Andreiev <alex@net.ck.ua>
2019-03-26 12:19:30 +0200

    update project

--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :track do
     sequence(:title) { |n| "Song_#{n}" }
-    file 'file'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,4 @@
 FactoryBot.define do
   factory :user do
-    name 'Hron'
-    email 'test@mail.com'
-    password_digest 'secret'
   end
 end
