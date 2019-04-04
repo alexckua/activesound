02d6b8910841ccfe844f4ae09459fc9919d706e8
Alex Andreiev <alex@net.ck.ua>
2019-04-04 11:56:46 +0300

    add artist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_228_184_208) do
+ActiveRecord::Schema.define(version: 20_190_404_083_956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'artists', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'tracks', force: :cascade do |t|
     t.string 'title'
