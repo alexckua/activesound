7876bc2755450faf82d309186ccab8995f03c79b
Alex Andreiev <alex@net.ck.ua>
2019-04-18 15:35:36 +0300

    uninstall grape

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
-  mount ApplicationAPI => '/api'
-  mount GrapeSwaggerRails::Engine => '/api/docs'
   post '/graphql', to: 'graphql#execute'
   root 'welcome#index'
 end
